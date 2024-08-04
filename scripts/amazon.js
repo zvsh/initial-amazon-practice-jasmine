bfd6248746009c151abd80d816545d0644d6dfdf
DevZVSH <zasheirali@gmail.com>
2024-08-04 12:16:27 -0700

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,6 +76,13 @@ if (matchingItem) {
     quantity: 1
    });
 }
-console.log(cart);
+
+let cartQuantity = 0;
+
+cart.forEach((item) => {
+  cartQuantity += item.quantity;
+});
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
