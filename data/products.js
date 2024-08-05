f83bac6d6357b150fb8dc4d19302fc7e3348194a
DevZVSH <zasheirali@gmail.com>
2024-08-05 15:43:26 -0700

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -59,6 +59,37 @@ class Clothing extends Product {
   }
 }
 
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+console.log(this);
+
+const object2 = {
+  a: 2,
+  b: this.a
+};
+*/
+
+/*
+function logThis() {
+  console.log(this);
+}
+logThis();
+logThis.call('hello');
+
+this
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+}
+object3.method();
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
