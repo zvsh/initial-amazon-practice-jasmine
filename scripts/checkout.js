c3ef697065e6a7857553e106ecdbed305af0b636
DevZVSH <zasheirali@gmail.com>
2024-08-04 22:24:54 -0700

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentsummary.js';
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 renderPaymentSummary();
