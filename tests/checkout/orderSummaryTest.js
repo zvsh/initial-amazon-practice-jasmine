7e4619bb98234615971faca7413b13a577a25fef
DevZVSH <zasheirali@gmail.com>
2024-08-05 18:29:48 -0700

    Convert loadProducts from XHR to fetch

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,6 +1,6 @@
 import {renderOrderSummary} from "../../scripts/checkout/orderSummary.js";
 import {loadFromStorage, cart} from '../../data/cart.js';
-import {loadProducts} from '../../data/products.js';
+import {loadProducts, loadProductsFetch} from '../../data/products.js';
 
 
 describe('test suite: renderOrdersummary', () => {
@@ -8,7 +8,7 @@ describe('test suite: renderOrdersummary', () => {
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
     
     beforeAll((done) => {
-      loadProducts(() => {
+      loadProductsFetch().then(() => {
         done();
       });
     });
