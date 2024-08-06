59d732f56ec8adc08942880f2f7f322366668665
DevZVSH <zasheirali@gmail.com>
2024-08-05 17:31:05 -0700

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,11 @@
 import {cart, addToCart} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {products, loadProducts} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
-let productsHTML = '';
+loadProducts(renderProductsGrid);
+
+function renderProductsGrid() {
+  let productsHTML = '';
 
 products.forEach((product) => {
   productsHTML += `
@@ -79,3 +82,4 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   updateCartQuantity();
   });
 });
+}
